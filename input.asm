48d83e7720de8bd5fe2b34307dcf7d29a63aa0bc
sizeru <nate.nick.will@gmail.com>
2024-11-28 18:12:00 -0600

    Have a correct implementation

--- input.asm
+++ input.asm
@@ -19,24 +19,22 @@ _start:
 	PUSH SP
 	PUSH -20
 	ADD
+
 	; Init SPM0 as *arr
-	PUSHA SPM0 4
+	PUSH SPM0
 	PUSH SP
 	ADD
-
-	PUSHA SPM1 4
-	PUSH 0
-	PUSH SPM0
-
 	; Init SPM1 as our iterator
-	PUSHA SPM1 4
+	PUSH SPM1
 	PUSH SPM0
 	ADD
+
 	; Fill array
 	; arr[0]
-	PUSHA SPM1 4
+	PUSHA SPM1 4 ; dst is 4 B
 	PUSH 40
-	ADD
+	ADD          ; write is auto
+	PUSH SPM1
 	PUSH SPM1
 	PUSH 4
 	ADD
@@ -45,12 +43,14 @@ _start:
 	PUSH 0
 	ADD
 	PUSH SPM1
+	PUSH SPM1
 	PUSH 4
 	ADD
 	; arr[2]
 	PUSHA SPM1 4
 	PUSH 7
 	ADD
+	PUSH SPM1
 	PUSH SPM1
 	PUSH 4
 	ADD
@@ -59,6 +59,7 @@ _start:
 	PUSH -500
 	ADD
 	PUSH SPM1
+	PUSH SPM1
 	PUSH 4
 	ADD
 	; arr[4]
@@ -66,67 +67,75 @@ _start:
 	PUSH 3000
 	ADD
 
+	; Pre call preparation (insn 47)
+	PUSH SPM1
+	PUSH SPM0
+	ADD ; SPM1 = arr
+	PUSH SPM0
+	PUSH 5
+	ADD ; SPM0 = size
+	PUSH ANC
+	PUSH 2
+	ADD
 
 sum:
 	; Args in SPM regs
 	; SPM-2: int size
 	; SPM-1: int arr[]
 
-	; SP -= 8
+	; SP -= 8 (insn 56)
 	PUSH SP   ; dst = r0 = SP
 	PUSH SP   ; op1 = r0 = SP
 	PUSH -8  ; op2 = -8
-	ADD 0       ; Unsized add
+	ADD       ; Unsized add
 
-	; total = 0
+	; total = 0 (referenced via sp) (insn 60)
 	PUSHA SP 4  ; dst = *r0 = *SP
-	ADD 0       ; Unsized add
+	ADD         ; writes 0s
 
-	; Init SPM0 as &i
-	PUSHA SPM0 4  ; dst = SPM0
+	; Init SPM0 as &i (sp + 4) (insn 63)
+	PUSH SPM0 ; dst = SPM0
 	PUSH SP   ; op1 = r0 = SP
-	PUSH 4   ; op2 = 4
-	ADD 0       ; 4B Add
-	; i = 0
+	PUSH 4    ; op2 = 4
+	ADD       ;  Add
+	; i = 0 (cycle 67)
 	PUSHA SPM0 4 ; dst = *SPM0 = i
 	ADD 0       ; 4B Add
 
-	; exit loop if i > size
+
+.loop_begin:
+
+	; The guard - exit loop if i > size (cycle 70)
 	PUSH 2   ; cond = 2 = LTE
 	PUSH.S -2   ; op1 = SPM-2 = size
 	PUSHA SPM0 4  ; op2 = SPM0 = i
-	JUMP .loop_begin     ; to .loop_end. factors size of self.
+	JUMP .loop_end     ; offset factors size of self.
 
-.loop_begin:
 	; SPM1 = arr+i
-	PUSH.S 1   ; dst = SPM1
-	PUSH.S -1  ; op1 = SPM-1 = arr
-	PUSH.S 0   ; op2 = SPM0 = i
-	PUSH.I 2   ; shift = 2
+	PUSH SPM1   ; dst = SPM1
+	PUSH SPM-1  ; op1 = SPM-1 = arr
+	PUSH SPM0   ; op2 = SPM0 = i
+	PUSH 2      ; shift = 2
 	ADD 4       ; 4B Add
 	; total += *(SPM1)
-	DSIZE 1
-	PUSH.R 0   ; dst = *r0 = *SP
-	DSIZE 1
-	PUSH.R 0   ; op1 = *r0 = *SP
-	DSIZE 1
-	PUSH.S 1   ; op2 = SPM1 = *(arr+i)
+	PUSHA SP 1  ; dst = *r0 = *SP
+	PUSHA SP 1  ; op1 = *r0 = *SP
+	PUSHA SPM1 4 ; op2 = SPM1 = *(arr+i)`
 	ADD 4       ; 4B Add
 	; i ++
-	DSIZE 1
-	PUSH.S 0   ; dst = SPM0 = i
-	DSIZE 1
-	PUSH.S 0   ; op1 = SPM0 = i
-	PUSH.I 1   ; op2 = 1
-	ADD 4
+	PUSHA SPM0 4 ; dst = SPM0 = i
+	PUSHA SPM0 4 ; op1 = SPM0 = i
+	PUSH 1   ; op2 = 1
+	ADD
 	JUMP .loop_begin    ; to .loop_begin
 
 .loop_end:
 	; SP += 8
-	PUSH.R 0   ; dst = r0 = SP
-	PUSH.R 0   ; op1 = r0 = SP
-	PUSH.I 8   ; op2 = 8
-	ADD 0       ; Unsized add
+	; PUSH.R 0   ; dst = r0 = SP
+	; PUSH.R 0   ; op1 = r0 = SP
+	; PUSH.I 8   ; op2 = 8
+	; ADD 0       ; Unsized add
+
 	; return
 	PUSH.R 2   ; target = r2 = LR
 	JUMP.ABS 3  ; TODO: Is this 3 (LE)?
